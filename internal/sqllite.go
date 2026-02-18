f63a55363782bf71d53665fc35e0a9014b94194f
Fernando Fragateiro <Fernando.fragateiro@emma-sleep.com>
2026-02-18 12:05:12 +0000

    Update SQLLite implementation

--- internal/sqllite.go
+++ internal/sqllite.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"os"
 	"synapsePlatform/internal/ingestor"
 
@@ -51,6 +52,32 @@ func (db *DB) runMigrations() error {
 	return nil
 }
 
-func (db *DB) StoreData(ctx context.Context, messages []ingestor.DeviceMessage) error {
+func (db *DB) StoreData(ctx context.Context, data *ingestor.NormalizedData) (generated.Event, error) {
+	// Serialize the data payload to JSON
+	dataJSON, err := json.Marshal(data)
+	if err != nil {
+		// todo
+	}
+
+	value := *data
+
+	savedEvent, err := db.Queries.CreateEvent(ctx, generated.CreateEventParams{
+		EventID:       value.Value().EventID.String(),
+		Domain:        value.Value().Domain,
+		EventType:     value.Value().EventType,
+		EntityID:      value.Value().EntityID,
+		EntityType:    value.Value().EntityType,
+		OccurredAt:    value.Value().OccurredAt,
+		IngestedAt:    value.Value().IngestedAt,
+		Source:        value.Value().Source,
+		SchemaVersion: value.Value().SchemaVersion,
+		Data:          string(dataJSON),
+		Metadata:      sql.NullString{},
+	})
+	if err != nil {
+		// todo
+	}
+
+	return savedEvent, nil
 
 }
